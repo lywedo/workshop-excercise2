f9138bccde30562881973fd19f6d7fd9bb98ae75
Anirudh Ayyagari <ayyagari.anirudh@gmail.com>
2023-09-19 22:27:13 +1200

    code fix

--- app.py
+++ app.py
@@ -1,72 +1,89 @@
+# Enhanced Streamlit Spotify Data Analysis with Additional Features
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_california_housing
 import pandas as pd
+import plotly.express as px
 
 
-@st.cache
+@st.cache_data
 def load_data():
     """
-    Load the California housing dataset and convert to DataFrame.
-
+     Load spotify dataset
     """
-    data = fetch_california_housing()
-    df = pd.DataFrame(data.data, columns=data.feature_names)
-    df["MedianHouseValue"] = data.target
-    return df
+    return pd.read_csv('spotify-2023.csv', encoding='ISO-8859-1')
 
 
-def filter_data_by_median_income(df, low_income, high_income):
-    """
-    Filter the dataset based on a range of median incomes.
-    """
-    return df[(df["MedInc"] >= low_income) & (df["MedInc"] <= high_income)]
-
-
+# Start of the Streamlit App
 def main():
     """
-    Setting up streamlit page configurations.
+        Function for exploratory data analysis
     """
-    st.set_page_config(page_title="California Housing Analysis", layout="wide")
-    st.title("California Housing Data Analysis")
-    # Load data
-    df = load_data()
+    # Load the Spotify dataset
+    data = load_data()
 
-    # Sidebar controls
-    st.sidebar.header("Filters")
-    min_income = float(np.min(df["MedInc"]))
-    max_income = float(np.max(df["MedInc"]))
-    low_income = st.sidebar.slider(
-        "Minimum Median Income", min_income, max_income, min_income, 0.1
-    )
-    high_income = st.sidebar.slider(
-        "Maximum Median Income", min_income, max_income, max_income, 0.1
-    )
-    filtered_df = filter_data_by_median_income(df, low_income, high_income)
+    # App title and introduction
+    st.title("Spotify Data Analysis (2023)")
+    st.write("Explore various insights from the Spotify 2023 dataset. Dive deeper using the interactive components.")
 
-    # Button to toggle between map and histograms
-    map_button = st.sidebar.button("Show Map")
-    if map_button:
-        # Display houses on a map of California
-        st.map(filtered_df.rename(columns={"Latitude": "lat",
-                                           "Longitude": "lon"}))
-    else:
-        # Display histograms for house values and median incomes
-        fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+    # Sidebar for data filtering
+    st.sidebar.header("Filter Data")
 
-        # Median House Value Histogram
-        ax[0].hist(filtered_df["MedianHouseValue"], bins=20, alpha=0.6, color="b")
-        ax[0].set_title("Distribution of Median House Values")
-        ax[0].set_xlabel("Median House Value (in $1000s)")
-        ax[0].set_ylabel("Number of Houses")
-        # Median Income Histogram
-        ax[1].hist(filtered_df["MedInc"], bins=20, alpha=0.6, color="r")
-        ax[1].set_title("Distribution of Median Income")
-        ax[1].set_xlabel("Median Income")
-        ax[1].set_ylabel("Number of Houses")
+    # Year Filter
+    selected_year = st.sidebar.slider("Select a Release Year", int(data["released_year"].min()), int(data["released_year"].max()), (int(data["released_year"].min()), int(data["released_year"].max())))
+    data = data[(data["released_year"] >= selected_year[0]) & (data["released_year"] <= selected_year[1])]
 
-        st.pyplot(fig)
+    # Artist Filter
+    unique_artists = sorted(data['artist(s)_name'].unique().tolist())
+    selected_artist = st.sidebar.multiselect("Select Artist(s)", unique_artists)
+    if selected_artist:
+        data = data[data['artist(s)_name'].isin(selected_artist)]
+
+    # Data Overview Button
+    if st.button("Show Data Overview"):
+        st.subheader("Data Overview")
+        st.write(data.head())
+
+    # Track Streams Distribution
+    st.subheader("Distribution of Track Streams")
+    fig_streams = px.histogram(data, x="streams", title="Streams Distribution", color_discrete_sequence=["#316394"])
+    st.plotly_chart(fig_streams)
+
+    # Number of Tracks per Artist
+    st.subheader("Top Artists by Track Count")
+    top_artists = data['artist(s)_name'].value_counts().head(10)
+    fig_artists = px.bar(top_artists, x=top_artists.index, y=top_artists.values, title="Top 10 Artists by Track Count", color_discrete_sequence=["#EF553B"])
+    st.plotly_chart(fig_artists)
+
+    # Track Attribute Distribution
+    st.subheader("Track Attribute Distribution")
+    attribute = st.selectbox("Select an attribute", ["danceability_%", "energy_%", "valence_%", "acousticness_%", "liveness_%", "speechiness_%"])
+    fig_attribute = px.histogram(data, x=attribute, title=f"Distribution of {attribute}", color_discrete_sequence=["#00CC96"])
+    st.plotly_chart(fig_attribute)
+
+    # BPM Distribution
+    st.subheader("BPM (Beats Per Minute) Distribution")
+    bpm_range = st.slider("Select a BPM range", int(data["bpm"].min()), int(data["bpm"].max()), (80, 140))
+    filtered_data = data[(data["bpm"] >= bpm_range[0]) & (data["bpm"] <= bpm_range[1])]
+    fig_bpm = px.histogram(filtered_data, x="bpm", title=f"BPM Distribution ({bpm_range[0]} - {bpm_range[1]})", color_discrete_sequence=["#AB63FA"])
+    st.plotly_chart(fig_bpm)
+
+    # Popular Keys Distribution
+    st.subheader("Popular Keys Distribution")
+    key_counts = data["key"].value_counts()
+    fig_keys = px.bar(key_counts, x=key_counts.index, y=key_counts.values, title="Distribution of Tracks by Key", color_discrete_sequence=["#FFA15A"])
+    st.plotly_chart(fig_keys)
+
+    # Mode Distribution
+    st.subheader("Mode Distribution")
+    mode_counts = data["mode"].value_counts()
+    fig_mode = px.pie(values=mode_counts.values, names=mode_counts.index, title="Distribution of Tracks by Mode", color_discrete_sequence=["#636EFA", "#FFA15A"])
+    st.plotly_chart(fig_mode)
+
+    # Interactive Scatter Plot
+    st.subheader("Scatter Plot of Attributes")
+    attr_x = st.selectbox("Select attribute for X-axis", ["danceability_%", "energy_%", "valence_%"], index=0)
+    attr_y = st.selectbox("Select attribute for Y-axis", ["danceability_%", "energy_%", "valence_%"], index=1)
+    fig_scatter = px.scatter(data, x=attr_x, y=attr_y, color="key", title=f"{attr_x} vs. {attr_y} by Key")
+    st.plotly_chart(fig_scatter)
 
 
 if __name__ == "__main__":
